6c18e752408a9d36c5750b3fb468cda250c386c6
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-10-02 17:36:30 +0100

    final submission for the block of code task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
-  trueOrFalse = false;
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    // task = true;
+    // task2 = false;
   }
 
   return [task, task2];
