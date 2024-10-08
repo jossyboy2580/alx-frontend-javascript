7835a901a7711e9e7894aea53860c76123c38682
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-10-08 23:12:45 +0100

    fixed the eslint errors for the task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
-export default function getListStudents () {
-    return [
-	{
-	    id: 1,
-	    firstname: "Guillaume",
-	    location: "San Francisco"
-	},
-	{
-	    id: 2,
-	    firstname: "James",
-	    location: "Columbia"
-	},
-	{
-	    id: 3,
-	    firstname: "Serena",
-	    location: "San Francisco"
-	}
-    ]
+export default function getListStudents() {
+  return [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstname: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
