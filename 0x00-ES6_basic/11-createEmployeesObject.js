4d9182843d55b584d960ccd1be9009b0b792050a
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-11-07 23:41:27 +0100

    creating an object and returning it has never been easier

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -2,7 +2,7 @@ export default function createEmployeesObject(departmentName, employees) {
   const newObject = {};
 
   newObject[`${departmentName}`] = [];
-  
+
   for (const employee of employees) {
     newObject[`${departmentName}`].push(employee);
   }
