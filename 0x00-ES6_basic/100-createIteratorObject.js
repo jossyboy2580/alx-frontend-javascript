77408abe8b2ff77052ba8bc8fa7d7e5e1eeb2992
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-11-10 23:18:50 +0100

    finally resolved 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,31 +1,29 @@
 export default function createIteratorObject(report) {
   let allEmployees = [];
 
-  // Flatten the array of employees
   for (const value of Object.values(report.allEmployees)) {
-    allEmployees = [ ...allEmployees, ...value ];
+    allEmployees = [...allEmployees, ...value];
   }
 
-  let ind = 0;
+  let ind = -1;
 
-  // Return an object with Symbol.iterator implemented correctly
   return {
     [Symbol.iterator]() {
-      // The iterator function should return an object with a next() method
       return {
-	next() {
-	  if (ind < allEmployees.length) {
-	    return {
-	      value: allEmployees[ind++],
-	      done: false
-	    };
-	  }
-	  return {
-	    value: undefined,
-	    done: true
-	  };
-	}
+        next() {
+          ind += 1;
+          if (ind < allEmployees.length) {
+            return {
+              value: allEmployees[ind],
+              done: false,
+            };
+          }
+
+          return {
+            done: true,
+          };
+        },
       };
-    }
+    },
   };
 }
