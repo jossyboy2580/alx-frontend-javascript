743a783d22d2ccbc6509f31751f2e98475d5dcb7
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-11-16 06:41:30 +0100

    apparently i was supposed to collectively resolve all the promises using promise.all instead of chaining, i hope the checker passes now

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  createUser()
-    .then((userData) => uploadPhoto().then((photoInfo) => ({ userData, photoInfo })))
-    .then(({ userData, photoInfo }) => {
+  Promise.all([createUser(), uploadPhoto()])
+    .then(([ userData, photoInfo]) => {
       console.log(`${photoInfo.body} ${userData.firstName} ${userData.lastName}`);
     })
     .catch(() => {
