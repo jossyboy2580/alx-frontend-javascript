5796e0135791c0961b2b332f2b7f6a5f262e5d44
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-11-16 06:06:12 +0100

    another trial at the 3-all.js submission

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   createUser()
-    .then((userData) => uploadPhoto()
-      .then((photoInfo) => ({ userData, photoInfo })))
+    .then((userData) => uploadPhoto().then((photoInfo) => ({ userData, photoInfo })))
     .then(({ userData, photoInfo }) => {
       console.log(`${photoInfo.body} ${userData.firstName} ${userData.lastName}`);
     })
