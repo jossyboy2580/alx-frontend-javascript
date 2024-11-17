5a9d80a2db9ffce7159b17bc88b9e1df5ed0e534
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-11-17 02:10:01 +0100

    Promise.allSettled again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,19 @@ import uploadPhoto from './5-photo-reject';
 
 export default function hadleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results)
+    .then((results) => {
+      return results.map((result) => {
+	if (result.status == 'fulfilled') {
+	  return {
+	    status: result.status,
+	    value: result.value
+	  };
+	} else {
+	  return {
+	    status: result.status,
+	    value: result.reason
+	  };
+	}
+      });
+    })
 }
